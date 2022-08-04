9dded5acaededdf6f9b9a4b8bc9022fe5b468e44
Shaik Wasef <wasef@regology.com>
2022-08-04 14:50:19 +0530

    initial code for Migration

--- migrationScriptWidgets.js
+++ migrationScriptWidgets.js
@@ -1,10 +1,23 @@
 var fs = require('fs');
 
-const cubesCollection = require('./reg_dashboard_cubes_populated');
+const cubesCollection = require('./reg_dashboard_cubes_mongo');
 const filtersCollection = require('./reg_dashboard_filters');
 const widgetsCollection = require('./reg_dashboard_widgets');
 
-const updatedWidgetsCollection = widgetsCollection.filter(item => item["filters"]).map((item) => {
+let updatedWidgetsCollection = widgetsCollection.map((item) => {
+	let cube = item["cube"];
+	for(let i = 0 ; i < cubesCollection.length ; i++ ){
+		if(cubesCollection[i]["id"] === cube ){
+				cube = cubesCollection[i]["_id"];
+				break;
+			}
+	}
+	const tenantId  = "60bdf4253f08118fcef4f30a";
+	item = {...item  , "cube" : cube , "tenantId" : tenantId }
+	return item;
+})
+
+updatedWidgetsCollection.filter(item => item["filters"]).forEach((item) => {
 	let itemFilters = item.filters;
 	itemFilters.forEach((itemFilter) => {
 		for(let i = 0 ; i < filtersCollection.length ; i++){
@@ -13,11 +26,10 @@ const updatedWidgetsCollection = widgetsCollection.filter(item => item["filters"
 			}
 		}
 	})
-	const cube = item["cube"];
-	cubesCollection
-	return item;
 })
 
-// // fs.writeFile("reg_dashboard_widgets_populated.json",JSON.stringify(widgetsCollection),function(err, result) {
-// //     if(err) console.log('error', err);
-// // });
+
+
+fs.writeFile("reg_dashboard_widgets_post_script.json",JSON.stringify(updatedWidgetsCollection),function(err, result) {
+    if(err) console.log('error', err);
+});
